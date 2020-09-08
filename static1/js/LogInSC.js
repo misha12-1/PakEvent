022d400d0c1652d2f2f134419e9b27591bebf910
misha12-1 <70949473+misha12-1@users.noreply.github.com>
2020-09-08 17:30:26 +0500

    Add files via upload

---
+++ static1/js/LogInSC.js
@@ -0,0 +1,17 @@
+document.querySelector('#img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+
+document.querySelector('.img-btn2').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
